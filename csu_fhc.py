9dc16477697d522344dc5ff6edd8fd20613789fd
Brenda <bdolan@atmos.colostate.edu>
2017-05-12 11:14:37 -0600

    Adding neceesray files for running the code.

---
+++ csu_fhc.py
@@ -0,0 +1,254 @@
+"""
+csu_fhc.py
+
+Brody Fuchs, CSU, Sept 2014
+brfuchs@atmos.colostate.edu
+
+Porting over Brenda Dolan's HID code from IDL 
+(apparently originally from Kyle Wiens)
+
+Modifications by Timothy Lang
+tjlangoc@gmail.com
+1/21/2015
+
+"""
+
+import numpy as np
+from beta_functions import get_mbf_sets_summer
+
+DEFAULT_WEIGHTS = {'DZ': 1.5, 'DR': 0.8, 'KD': 1.0, 'RH': 0.8, 'LD': 0.5,
+                   'T': 0.4}
+
+def hid_beta(x_arr, a, b, m):
+    """Beta function calculator"""
+    #print a
+    return 1.0/(1.0 + ( ((x_arr - m)/a)**2)**b)
+
+def csu_fhc_summer(weights=DEFAULT_WEIGHTS, method='hybrid',
+                   dz=None, zdr=None, ldr=None, kdp=None, rho=None, T=None,
+                   verbose=False, plot_flag=False, n_types=10, temp_factor=1,
+                   band='S',use_temp=False,CSV_dir='./beta_function_parameters/'):
+    """
+    Does FHC for warm-season precip.
+    
+    Arguments:
+    use_temp = Set to False to not use T in HID
+    weights = Dict that contains relative weights for every variable; see 
+              DEFAULT_WEIGHTS for expected stucture
+    method = Currently support 'hybrid' or 'linear' methods; hybrid preferred
+    verbose = Set to True to get text updates
+    plot_flag = Flag to turn on optional beta function plots
+    band = 'C' or 'S'
+    temp_factor = Factor to modify depth of T effects; > 1 will broaden the 
+                  slopes of T MBFs
+    n_types = Number of hydrometeor species
+    verbose = Set to True to get text updates
+
+    Input measurands (if not None, all must match in shape/size):
+    dz = Input reflectivity scalar/array
+    zdr = Input reflectivity scalar/array
+    ldr = Input reflectivity scalar/array
+    kdp = Input reflectivity scalar/array
+    rho = Input reflectivity scalar/array
+    T = Input temperature scalar/array
+    
+    Returns:
+    mu = Input array + additional dimension containing weights for each HID species
+    To get dominant species number: fh = np.argmax(mu, axis=0) + 1
+
+    HID types:           Species #:
+    -------------------------------
+    Drizzle                  1
+    Rain                     2
+    Ice Crystals             3
+    Aggregates               4
+    Wet Snow                 5
+    Vertical Ice             6
+    Low-Density Graupel      7
+    High-Density Graupel     8
+    Hail			         9
+    Big Drops		         10
+    
+    """
+
+    print 'Using temp in HID:',use_temp
+    if dz is None:
+        print 'FHC fail, no reflectivity field'
+        return None
+    if T is None:
+        use_temp = False
+
+    #Populate fhc_vars and radar_data based on what was passed to function
+    radar_data, fhc_vars = _populate_vars(dz, zdr, kdp, rho, ldr, T, verbose)
+
+    #Now grab the membership beta function parameters
+    mbf_sets = get_mbf_sets_summer(use_temp=use_temp, plot_flag=plot_flag,
+               n_types=n_types, temp_factor=temp_factor, band=band,
+               verbose=verbose,fdir = CSV_dir)
+    sets = _convert_mbf_sets(mbf_sets)
+
+    #Check for presence of polarimetric variables
+    pol_flag = _get_pol_flag(fhc_vars)
+
+    #Check for presence of temperature
+    if use_temp:
+        if verbose:
+            print 'Using T in FHC'
+    else:
+        fhc_vars['T'] = 0
+        if verbose:
+            print 'Not using T in FHC'
+
+    #Get weighted sums\
+    #print np.shape(dz)
+    #print 'Getting weight_sums'
+    weight_sum, varlist = _get_weight_sum(fhc_vars, weights, method, verbose)
+    if weight_sum is None:
+        return None
+
+    #Now loop over every hydrometeor class
+    #print 'Now looping over hydrometeors'
+    ##print np.shape(radar_data)
+    test_list = _get_test_list(fhc_vars, weights, radar_data, sets, varlist,
+                               weight_sum, pol_flag, use_temp, method)
+    #print 'Got testlist. Dunnow what the hangup is'
+    if test_list is None:
+        return None
+
+    #Finish up
+    mu = np.array(test_list)
+    if verbose:
+        print mu.shape
+        print 'mu max: ', mu.max()
+    return mu
+
+#########################
+#Private Functions Below#
+#########################
+
+def _convert_mbf_sets(mbf_sets):
+    """Simply gets mbf_sets dict into form that matches labels used in csu_fhc"""
+    sets = {}
+    sets['DZ'] = mbf_sets['Zh_set']
+    sets['DR'] = mbf_sets['Zdr_set']
+    sets['KD'] = mbf_sets['Kdp_set']
+    sets['LD'] = mbf_sets['LDR_set']
+    sets['RH'] = mbf_sets['rho_set']
+    sets['T'] = mbf_sets['T_set']
+    return sets
+
+def _get_pol_flag(fhc_vars):
+    """Check for presence of polarimetric variables"""
+    if fhc_vars['DR'] or fhc_vars['KD'] or fhc_vars['LD'] or fhc_vars['RH']:
+        pol_flag = True
+    else:
+        pol_flag = False
+    return pol_flag
+
+def _populate_vars(dz, zdr, kdp, rho, ldr, T, verbose):
+    """Check for presence of each var, and update dicts as needed"""
+    fhc_vars = {'DZ': 1, 'DR': 1, 'KD': 1, 'LD': 1, 'RH': 1, 'T': 1}
+    radar_data = {}
+    if dz is not None:
+        radar_data['DZ'] = dz
+    if zdr is not None:
+        radar_data['DR'] = zdr
+    if kdp is not None:
+        radar_data['KD'] = kdp
+    if rho is not None:
+        radar_data['RH'] = rho
+    if ldr is not None:
+        radar_data['LD'] = ldr
+    if T is not None:
+        radar_data['T'] = T
+    if 'DR' not in radar_data.keys():
+        fhc_vars['DR'] = 0
+    if 'KD' not in radar_data.keys():
+        fhc_vars['KD'] = 0
+    if 'LD' not in radar_data.keys():
+        fhc_vars['LD'] = 0
+    if 'RH' not in radar_data.keys():
+        fhc_vars['RH'] = 0
+    if 'T' not in radar_data.keys():
+        fhc_vars['T'] = 0
+    if verbose:
+        print 'USING VARIABLES: ', fhc_vars
+    return radar_data, fhc_vars
+
+def _get_weight_sum(fhc_vars, weights, method, verbose):
+    """Gets sum of weights and varlist used, which depend on method"""
+    if 'hybrid' in method:
+        if verbose:
+            print 'Using hybrid HID method. Pol vars weighted,',\
+                      'Z and T (if used) are multiplied'
+        varlist = ['DR', 'KD', 'RH', 'LD']
+    elif 'linear' in method:
+        if verbose:
+            print 'NOT using hybrid, all variables treated as weighted sum'
+        varlist = ['DR', 'KD', 'RH', 'LD', 'T', 'DZ']
+    else:
+        print 'No weighting method defined, use hybrid or linear'
+        return None, None
+    weight_sum = np.sum(np.array([fhc_vars[key]*weights[key]
+                                 for key in varlist]))
+    if verbose:
+        print 'weight_sum: ', weight_sum
+    return weight_sum, varlist
+
+def _calculate_test(fhc_vars, weights, radar_data, sets, varlist, weight_sum, c):
+    """Loop over every var to get initial value for each HID species 'test'"""
+    
+    test = (np.sum(np.array([fhc_vars[key]*weights[key]*\
+            hid_beta(radar_data[key], sets[key]['a'][c],\
+            sets[key]['b'][c], sets[key]['m'][c])\
+            for key in varlist if key in radar_data.keys()]),\
+            axis = 0))/weight_sum
+    return test
+
+def _get_test_list(fhc_vars, weights, radar_data, sets, varlist, weight_sum,
+                   pol_flag, use_temp, method):
+    """
+    Master loop to compute HID values for each species ('test' & 'test_list').
+    Depending on method used, approach is modfied.
+    Currently disabling testing as it gets spoofed by bad data. Better to let the
+    calculations continue then mask out the bad data using other methods.
+    TO DO: Change poor naming scheme for variables 'test' and 'test_list']
+    """
+    #TJL - Check order of if statements
+    test_list = []
+    for c in range(len(sets['DZ']['m'])):
+        if 'hybrid' in method: #Hybrid emphasizes Z and T extra HARD
+            if pol_flag:
+                test = _calculate_test(fhc_vars, weights, radar_data, sets,
+                                       varlist, weight_sum, c)
+                #if test.max() > 1: #Maximum of test should never be more than 1
+                #    print 'Fail loc 1, test.max() =', test.max()
+                #    return None
+            if use_temp:
+                if pol_flag:
+                    #*= multiplies by new value and stores in test
+                    test *= hid_beta(radar_data['T'], sets['T']['a'][c],
+                                     sets['T']['b'][c], sets['T']['m'][c])
+                    #print 'in loc 2'
+                    #if test.max() > 1: #Maximum of test should never be > 1
+                    #    print 'Fail loc 2, test.max() =', test.max()
+                    #    return None
+                else:
+                    test = hid_beta(radar_data['T'], sets['T']['a'][c],
+                                    sets['T']['b'][c], sets['T']['m'][c])
+            if fhc_vars['DZ']:
+                if pol_flag or use_temp:
+                    test *= hid_beta(radar_data['DZ'], sets['DZ']['a'][c],
+                                     sets['DZ']['b'][c], sets['DZ']['m'][c])
+                    #if test.max() > 1: #Maximum of test should never be > 1
+                    #    print 'Fail loc 3, test.max() =', test.max()
+                    #    return None
+                else:
+                    test = hid_beta(radar_data['DZ'], sets['DZ']['a'][c],
+                                    sets['DZ']['b'][c], sets['DZ']['m'][c])
+        elif 'linear' in method: #Just a giant weighted sum
+            if pol_flag:
+                test = _calculate_test(fhc_vars, weights, radar_data, sets,
+                                       varlist, weight_sum, c)
+        test_list.append(test)
+    return test_list
---
+++ general_tools.py
@@ -0,0 +1,225 @@
+# This just houses some general functions
+
+import numpy as np
+from copy import deepcopy
+import scipy.ndimage as ND
+import datetime
+import matplotlib.dates as md
+import sys
+import linecache
+
+def percentile95(inputdata, **kwargs):
+    return np.percentile(inputdata, 95, **kwargs)
+
+def percentile99(inputdata, **kwargs):
+    return np.percentile(inputdata, 99, **kwargs)
+
+def percentile90(inputdata, **kwargs):
+    return np.percentile(inputdata, 90, **kwargs)
+
+def percentile1(inputdata, **kwargs):
+    return np.percentile(inputdata, 1, **kwargs)
+
+def percentile5(inputdata, **kwargs):
+    return np.percentile(inputdata, 5, **kwargs)
+
+def percentile10(inputdata, **kwargs):
+    return np.percentile(inputdata, 10, **kwargs)
+
+
+def print_exception():
+    exc_type, exc_obj, tb = sys.exc_info()
+    f = tb.tb_frame
+    lineno = tb.tb_lineno
+    filename = f.f_code.co_filename
+    linecache.checkcache(filename)
+    line = linecache.getline(filename, lineno, f.f_globals)
+    print 'EXCEPTION IN ({}, LINE {} "{}"): {}'.format(filename, lineno, line.strip(), exc_obj)
+
+
+def size_filter(input_arr, filter, min_volume = 1, filter_type = '!=', \
+			bad_val = -1, filter_out = True, filter_bad = -1, verbose = False):
+    "Used in a number of other functions, will filter out spurious data based on some filter"
+
+	# will do non-zero feature ID on some input_arr and return that feature output
+	# unless filter_arr is not None, in which case it will use that feature output to filter
+	# the filter_arr and return that
+    small_volumes = 0. # keep track of number of volumes filtered out
+    big_volumes = 0. # keep track of volumes that are big enough
+    label_structure = np.ones((3,3,3)) # allows all 3d neighbors for ID
+    out = deepcopy(input_arr) # copy of array to do filtering on and return
+    dummy_arr = deepcopy(input_arr) # make a dummy array the same as input to do stuff with
+#        bad = dummy_arr != filter # if not equal to filter, set it to 0 to not be ID'ed by ND label function
+	# this one gonna try to use strings and evals
+    bad = eval("dummy_arr %s filter"%filter_type) # if not equal to filter, set it to 0 to not be ID'ed by ND label function
+    dummy_arr[bad] = 0 # now have only one certain HID identifier
+    labelarray, nfeat = ND.label(dummy_arr, structure=label_structure)
+	#print '%d features'%nfeat
+        # now have the HIDs labelled
+    for feat in range(1, nfeat+1): # dont do 0
+    	points = np.where(labelarray == feat)
+	npoints = points[0].shape[0]
+        if npoints < min_volume:
+#                print 'FILTERING OUT A VOLUME'
+	    small_volumes += 1
+            labelarray[points] = bad_val # resetting the feature array to 0 if too small
+	    if filter_out: out.mask[points] = True
+	else: big_volumes += 1
+    if verbose:
+	print '%d volumes large enough, %d filtered out'%(big_volumes, small_volumes)
+    if filter_out:
+    	if verbose: print 'Returning filtered array'
+	return out
+    else: # actually use the labelarray as a filter to modify some other array
+	if verbose: print 'Returning feature array'
+	return labelarray # return the labelarray, mainly for debugging purposes
+
+#######################################################################################################################
+
+def nder (y, n, smooth=0, delta = 1, normalize = 1): # nth derivative using gradient recursively
+    out = np.zeros(y.shape) # out is same shape as input array
+    if smooth > 0:
+        for itime in range(y.shape[-1]): # loop thru time array
+
+            out[:,itime] = ND.gaussian_filter1d(y[:,itime],smooth) # first smooth it for each time
+            for i in range(n): # do n derivatives
+                out[:,itime] = np.array(np.gradient(out[:,itime], delta)) # convert gradient output to array
+            if normalize == 1:
+                if out[:,itime].any(): # if any are not 0, how to normalize?
+                    out[:,itime] = out[:,itime]/np.abs(out[:,itime]).max()
+    return out
+
+
+#######################################################################################################################
+
+
+def pretty_time(ax):
+    xfmt = md.DateFormatter('%H:%M')
+    ax.xaxis.set_major_formatter(xfmt)
+
+#######################################################################################################################
+
+
+def make_patch_spines_invisible(ax):
+    ax.set_frame_on(True)
+    ax.patch.set_visible(False)
+    for sp in ax.spines.itervalues():
+        sp.set_visible(False)
+#######################################################################################################################
+
+
+def shadow_line_plot(x, y, ax, fgcolor='white', bgcolor='black', fgwidth=1.5, bgwidth=2.5, alpha=1.0, **kwargs):
+    # kwargs can be passed to the foreground line plot
+    ax.plot(x, y, bgcolor, linewidth=bgwidth, alpha=alpha)
+    line_handle = ax.plot(x, y, fgcolor, linewidth=fgwidth, alpha=alpha, **kwargs)
+    return line_handle
+#######################################################################################################################
+
+
+
+def get_cells_times(cell_st, track_st, track_num, year=2011):
+
+    c = np.where(track_st.num == track_num)
+    track_cell = track_st[c]
+    cells = np.array([np.int(tc) for tc in track_cell.cells[0].split(',')[:-1]])
+    
+    times = []
+    for i in range(cells.shape[0]):
+        wh_cell = np.where(cell_st.num == cells[i])[0][0]
+        this_cell = cell_st[wh_cell]
+        jday = np.int(np.floor(this_cell.time))
+        hr, mn, sec = int(this_cell.time_str[0:2]), int(this_cell.time_str[3:5]), int(this_cell.time_str[6:8])
+        ftime = datetime.datetime(year,1,1) + datetime.timedelta(days = jday-1) + datetime.timedelta(hours = hr) + \
+                datetime.timedelta(minutes = mn) + datetime.timedelta(seconds = sec)
+        times.append(ftime)
+
+    return cells, np.array(times)
+
+#######################################################################################################################
+
+def hist2d(varx=None, vary=None, binsx=None, binsy=None):
+# This will just look at the whole volume
+
+    vars_q = (varx is not None) and (vary is not None)
+    bins_q = (binsx is not None) and (binsy is not None)
+
+    if vars_q:
+
+# grab all values and unravel them (if that's what it's called), but only between the valid indices
+        if (hasattr(varx, 'mask') and hasattr(vary, 'mask')):
+            outmask = np.logical_and(varx.mask, vary.mask)
+            varx.mask = outmask
+            vary.mask = outmask
+            varx_ravel = np.ma.compressed(varx)
+            vary_ravel = np.ma.compressed(vary)
+
+        else:
+            mskx = (np.isfinite(varx))
+            msky = (np.isfinite(vary))
+            msk = np.logical_and(mskx,msky)
+            varx_ravel = np.ravel(varx[msk])
+            vary_ravel = np.ravel(vary[msk])
+
+
+        if not bins_q:
+            binsx = 20
+            binsy = 20
+
+        print np.nanmax(vary_ravel)
+        print np.nanmax(varx_ravel)
+        hist, edges = np.histogramdd((vary_ravel, varx_ravel), bins=(binsy, binsx), normed=True)
+        hist *= 100.0/np.sum(hist)
+        print np.shape(edges)
+        return hist, edges
+
+
+
+    else:
+        print 'Need to provide proper variables to the varx and vary keywords'
+        return 
+
+#######################################################################################################################
+def make_cmap(colors, bit=False):
+    '''
+    make_cmap takes a list of tuples which contain RGB values. The rgb values
+    may either be in 8bit [0 to 255] (in which bit must be set to True when
+    called) or arithmetic [0 to 1] (default). make_cmap returns a cmap with
+    equally spaced colors.
+    Arange you tuples so that the first color is the lowest value for the
+    colorbar and the last is the highest.
+    '''
+    import matplotlib
+    bit_rgb = np.linspace(0,1,256)
+    position = np.linspace(0,1,len(colors))
+    if bit:
+        for i in range(len(colors)):
+            colors[i] = (bit_rgb[colors[i][0]],
+                         bit_rgb[colors[i][1]],
+                         bit_rgb[colors[i][2]])
+    cdict = {'red':[], 'green':[], 'blue':[]}
+    for pos, color in zip(position, colors):
+        cdict['red'].append((pos, color[0], color[0]))
+        cdict['green'].append((pos, color[1], color[1]))
+        cdict['blue'].append((pos, color[2], color[2]))
+
+    return matplotlib.colors.LinearSegmentedColormap('my_colormap',cdict,256)
+
+if __name__ == '__main__':
+    fig = plt.figure()
+    ax = fig.add_subplot(211)
+### Create a list of RGB tuples
+    colors = [(255,0,0), (255,255,0), (255,255,255), (0,157,0), (0,0,255)] # This example uses the 8-bit RGB
+### Call the function make_cmap which returns your colormap
+
+    my_cmap = make_cmap(colors, bit=True)
+### Use your colormap
+    plt.pcolor(np.random.rand(25,50), cmap=my_cmap)
+    plt.colorbar()
+
+    ax = fig.add_subplot(212)
+    colors = [(1,1,1), (0.5,0,0)] # This example uses the arithmetic RGB
+### If you are only going to use your colormap once you can
+### take out a step.
+    plt.pcolor(np.random.rand(25,50), cmap=make_cmap(colors))
+    plt.colorbar()
+    plt.show()
