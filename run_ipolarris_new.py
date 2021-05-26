8540049a88317d70697716016a76def0c7f38152
Anthony Di Stefano <adistefa@eoas.ubc.ca>
2021-05-26 10:45:11 -0700

    No issues re: exception for IndexError. Needed to change z variable in config file to match height data in radar files.

--- run_ipolarris_new.py
+++ run_ipolarris_new.py
@@ -112,8 +112,7 @@ else:
         if 'd' in rdata.data[rdata.z_name].dims:
             try:
                 whz = np.where(rdata.data[rdata.z_name].sel(d=i).values==config['z'])[0][0]
-            #except IndexError as ie:
-            except IndexError:
+            except IndexError as ie:
                 #print('checking z...',rdata.data[rdata.z_name].sel(d=i).values)
                 zdiffs = np.median(np.diff(rdata.data[rdata.z_name].values))
                 whz = np.where(np.isclose(rdata.data[rdata.z_name].sel(d=i).values,config['z'],rtol=zdiffs))[0][0]
