98e3c7074e720701d5ca5db406e930b6afd21edf
Anthony Di Stefano <adistefa@eoas.ubc.ca>
2021-12-10 17:48:16 -0800

    NEW! HID comparison cfads work and look nice

--- run_ipolarris_new.py
+++ run_ipolarris_new.py
@@ -99,13 +99,13 @@ if sys.argv[2:]:
  
     outdir = config2['image_dir']+'cfad_diff_individ/'
     os.makedirs(outdir,exist_ok=True)
-    
+        
     fig,ax = plot_driver.plot_difference_cfad(rdata,rdata2,rdata.dz_name,rdata2.dz_name,config,bins=config['dzbins'],xlab=rdata.longnames[rdata.dz_name]+' '+rdata.units[rdata.dz_name],cscfad=False,xlim=[np.min(config['dzbins']),np.max(config['dzbins'])+1],ylim=config['zlim'],nor=10)
     ax[0].set_title(rdata.exper,fontsize=16,fontweight='bold')
     ax[0].set_ylabel('Height (km MSL)',fontsize=16)
     ax[1].set_title(rdata2.mphys.upper(),fontsize=16,fontweight='bold')
     ax[3].set_title('({e} - {v})'.format(e=rdata.exper,v=rdata2.mphys.upper()),fontsize=16,fontweight='bold')    
-    plt.savefig('{i}{e}_{m}_{v}_CFAD_diff.{p}'.format(p=config2['ptype'],i=outdir,e=rdata2.exper,m=rdata2.mphys,v=rdata.dz_name),dpi=400,bbox_inches='tight')
+    plt.savefig('{i}{e}_{m}_{v}_CFAD_diff.{p}'.format(p=config2['ptype'],i=outdir,e=rdata2.exper,m=rdata2.mphys.upper(),v=rdata.dz_name),dpi=400,bbox_inches='tight')
     plt.close(fig)
 
     print('\nDone! Saved to '+outdir)
@@ -117,7 +117,7 @@ if sys.argv[2:]:
     ax[0].set_ylabel('Height (km MSL)',fontsize=16)
     ax[1].set_title(rdata2.mphys.upper(),fontsize=16,fontweight='bold')
     ax[3].set_title('({e} - {v})'.format(e=rdata.exper,v=rdata2.mphys.upper()),fontsize=16,fontweight='bold')    
-    plt.savefig('{i}{e}_{m}_{v}_CFAD_diff.{p}'.format(p=config2['ptype'],i=outdir,e=rdata2.exper,m=rdata2.mphys,v=rdata.zdr_name),dpi=400,bbox_inches='tight')
+    plt.savefig('{i}{e}_{m}_{v}_CFAD_diff.{p}'.format(p=config2['ptype'],i=outdir,e=rdata2.exper,m=rdata2.mphys.upper(),v=rdata.zdr_name),dpi=400,bbox_inches='tight')
     plt.close(fig)
 
     print('\nDone! Saved to '+outdir)
@@ -129,7 +129,7 @@ if sys.argv[2:]:
     ax[0].set_ylabel('Height (km MSL)',fontsize=16)
     ax[1].set_title(rdata2.mphys.upper(),fontsize=16,fontweight='bold')
     ax[3].set_title('({e} - {v})'.format(e=rdata.exper,v=rdata2.mphys.upper()),fontsize=16,fontweight='bold')    
-    plt.savefig('{i}{e}_{m}_{v}_CFAD_diff.{p}'.format(p=config2['ptype'],i=outdir,e=rdata2.exper,m=rdata2.mphys,v=rdata.kdp_name),dpi=400,bbox_inches='tight')
+    plt.savefig('{i}{e}_{m}_{v}_CFAD_diff.{p}'.format(p=config2['ptype'],i=outdir,e=rdata2.exper,m=rdata2.mphys.upper(),v=rdata.kdp_name),dpi=400,bbox_inches='tight')
     plt.close(fig)
 
     print('\nDone! Saved to '+outdir)
@@ -137,13 +137,11 @@ if sys.argv[2:]:
     print('\nPlotting composites by time for variable '+rdata.hid_name+'...')
      
     #fig,ax = plot_driver.plot_difference_cfad(rdata,rdata2,rdata.w_name,rdata2.w_name,'Vertical Velocity',config,config2,bins=np.arange(-20,21,1),savefig=True,cscfad=False)
-
+    
     fig,ax = plot_driver.plot_hid_comparison_cfad(rdata,rdata2,config=config,cscfad=None)
-    #ax[0].set_title(rdata.exper,fontsize=16,fontweight='bold')
-    #ax[0].set_ylabel('Height (km MSL)',fontsize=16)
-    #ax[1].set_title(rdata2.mphys.upper(),fontsize=16,fontweight='bold')
-    #ax[3].set_title('({e} - {v})'.format(e=rdata.exper,v=rdata2.mphys.upper()),fontsize=16,fontweight='bold')    
-    plt.savefig('{i}{e}_{m}_{v}_CFAD_diff.{p}'.format(p=config2['ptype'],i=outdir,e=rdata2.exper,m=rdata2.mphys,v=rdata.hid_name),dpi=400,bbox_inches='tight')
+    ax[0].set_title(rdata.exper,fontsize=16,fontweight='bold')
+    ax[1].set_title(rdata2.mphys.upper(),fontsize=16,fontweight='bold')
+    plt.savefig('{i}{e}_{m}_{v}_CFAD_diff.{p}'.format(p=config2['ptype'],i=outdir,e=rdata2.exper,m=rdata2.mphys.upper(),v=rdata.hid_name),dpi=400,bbox_inches='tight')
     plt.close(fig)
 
     '''
@@ -157,6 +155,7 @@ if sys.argv[2:]:
 
     fig,ax = plot_driver.plot_hid_comparison_cfad(rdata,rdata2,config=config,cscfad='convective',savefig=True)
     '''
+    print('\niPOLARRIS RUN COMPLETE FOR '+config['mphys'].upper()+' '+config['sdatetime']+' - '+config['edatetime']+'\n')
 
 ################################################################################
 ################## Now you can just start plotting! ############################
